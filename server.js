127fc5177c75c0f850f3a638e9275e3bd62d89fa
XuyangJia <jiaxuyang@outlook.com>
2023-09-06 18:29:30 +0800

    更换包管理器

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import 'dotenv/config.js'
+import 'dotenv/config'
 import {env} from 'process'
 import app from './app.js'
 
-console.log(env.NODE_ENV)
 const PORT = env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
