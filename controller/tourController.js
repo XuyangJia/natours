bc9df67e69cccb6fe50c7c16b830d629fc637b0a
XuyangJia <jiaxuyang@outlook.com>
2023-09-09 02:05:20 +0800

    fat Models

--- controller/tourController.js
+++ controller/tourController.js
@@ -96,6 +96,89 @@ const deleteTour = async (req, res) => {
   }
 }
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: -1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ])
+    res.status(200).json({
+      status: 'success',
+      data: { stats }
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    })
+  }
+}
+
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0 },
+      },
+      {
+        $sort: { numToursStarts: -1 },
+      },
+      {
+        $limit: 3,
+      },
+    ])
+    res.status(200).json({
+      status: 'success',
+      data: { plan }
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    })
+  }
+}
+
 export default {
   getAllTours,
   createTour,
@@ -103,4 +186,6 @@ export default {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 }
--- router/tourRoutes.js
+++ router/tourRoutes.js
@@ -4,6 +4,8 @@ import tourController from '../controller/tourController.js'
 const router = express.Router()
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
 
 router
   .route('/')
