d149c27b6d704a12f9398d3cbb0bd9d0e8b115fe
XuyangJia <jiaxuyang@outlook.com>
2023-09-14 15:26:46 +0800

    uncaught Exception

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import process from 'process'
 import mongoose from 'mongoose'
 import app from './app.js'
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...')
+  console.log(`Name:"${err.name}", Message:"${err.message}"`)
+  process.exit(1)
+})
+
 const { PORT = 3000, DATABASE_URI, DATABASE_PASSWORD } = process.env
 const uri = DATABASE_URI.replace('<PASSWORD>', DATABASE_PASSWORD)
 mongoose
@@ -16,8 +22,8 @@ const server = app.listen(PORT, () => {
 })
 
 process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...')
   console.log(`Name:"${err.name}", Message:"${err.message}"`)
-  console.log('UNHANDLED REJECTION! Server is shutting down...')
   server.close(() => {
     process.exit(1)
   })
